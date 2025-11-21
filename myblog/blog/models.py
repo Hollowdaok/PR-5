b4b1b7494a4039cc68b8f1dd9db43791f7b49b47
Hollowdaok <didilivsk@gmail.com>
2025-11-21 08:28:33 +0200

    Update models.py

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -16,7 +16,7 @@ class Post(models.Model):
     objects = models.Manager()
     published = PublishedManager()
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)

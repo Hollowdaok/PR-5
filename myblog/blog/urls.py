7a805e87925ee0a46803a9b68778bdba7d1d1f0f
Hollowdaok <didilivsk@gmail.com>
2025-11-28 09:56:43 +0200

    Update feeds.py

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.contrib.sitemaps import Sitemap
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'
 sitemaps = {
@@ -16,4 +17,5 @@ urlpatterns = [
     path('post/<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+    path('feed/', LatestPostsFeed(), name='post_feed')
 ]
